415bcf80718f4bbe9bb196fcfe924da8d9290a33
Fabián de Jesús de Lira Rodríguez <Fabiannawal@502gmail.com>
2025-12-03 12:06:33 -0600

    Se agrego la funcion de division

--- ExamenPractico/Calculadora.java
+++ ExamenPractico/Calculadora.java
@@ -1,3 +1,13 @@
 public class Calculadora {
     
+  public int dividir(int a, int b) {
+      if (b == 0) {
+          throw new IllegalArgumentException("No se puede dividir entre cero");
+    }
+      return a / b;
+  }
+
+
+
+
 }
