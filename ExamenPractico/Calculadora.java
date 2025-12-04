b841b6d2faa06c08b999d51b05e617645b8de9db
up240527-ops <up240527@alumnos.upa.edu.mx>
2025-12-04 11:09:17 -0600

    Se agregaron las funciones faltantes a el objeto calculadora

--- ExamenPractico/Calculadora.java
+++ ExamenPractico/Calculadora.java
@@ -3,8 +3,14 @@ public class Calculadora {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
 
-        int resultado = calc.sumar(10, 2);
-        System.out.println("Resultado: " + resultado);
+        int resSuma = calc.sumar(10, 2);
+        int resResta = calc.sumar(10, 2);
+        int resMulti = calc.sumar(10, 2);
+        int resDiv = calc.sumar(10, 2);
+        System.out.println("Suma: " + resSuma);
+        System.out.println("Resta: " + resResta);
+        System.out.println("Multiplicacion: " + resMulti);
+        System.out.println("Division: " + resDiv);
     }
 
     public int sumar(int a, int b) {
